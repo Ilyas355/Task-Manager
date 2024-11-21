c5d1cfb1de895ae11d5083645cbc928455a41781
Ilyas Hussein <husseinilyas5@gmail.com>
2024-11-21 18:12:20 +0000

    feature: prompt user to select a choice and apply the correct process based on their choice in View Due Tasks function

--- run.py
+++ run.py
@@ -385,6 +385,47 @@ def viewDueTasks():
             f"Status: {DueTaskList[x][1]},\nDue Date: {DueTaskList[x][2]}\n"
         )
 
+    print("Enter the number of the task below to select a task:")
+    print("Enter 0 to return to the main menu:\n")
+    choice = input("")
+
+    try:
+        if choice == str(0):
+            main()
+            return
+        selection = DueTaskList[int(choice) - 1]
+        valid=False
+        while valid is False:
+            clear()
+            print("View Due Tasks:\n")
+            print(
+                f"Task: {choice},\nContent: {selection[0]},\n"
+                f"Status: {selection[1]},\nDue Date: {selection[2]}\n"
+            )
+            print("\nTo modify the task, enter 1")
+            print("To remove the task, enter 2")
+            print("To return to the main menu, enter 0\n")
+            choice2=input("")
+            if choice2 == str(0):
+                main()
+                valid = True
+            if choice2 == str(1):
+                print(selection)
+                taskObject = Task(selection[0],selection[1],selection[2])
+                modifyTask(taskObject, selection)
+                valid = True
+            elif choice2 == str(2):
+                removeTask(selection)
+                valid = True
+            else:
+                print('Invalid input, please enter a correct value:')
+                time.sleep(1)
+            
+    except IndexError as e:
+        print(f'{e}, please enter the correct value:')
+        time.sleep(1.5)
+        viewDueTasks()
+        return
 
 
 def viewPastDueTasks():
